bf75afe730371b75513da5a02c317e602fcb3490
bbanar3 <88082353+bbanar3@users.noreply.github.com>
2021-07-28 15:36:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,28 @@ import pad_image_2 from "./color_2d_plot2.png";
 import image_architecture from "./figure_with_LSR.png";
 import ReactAudioPlayer from 'react-audio-player';
 import { ImageBackground} from "react-native";
+import { useState, useEffect } from 'react';
 
 
-
-var metric1 = 301;
-var metric2 = 301;
+var metric1 = 151;
+var metric2 = 151;
 var metric3 = 151;
 var metric4 = 151;
 
-var source_pianoroll_file_name = "test_midi_original.png";
+var source_pianoroll_file_name = "midi_1_1_5_5.png";
 var source_mp3_file_name = "test_midi_original.mp3";
 
+var original_page_width = 1920;
+var original_page_height = 906;
 
+
+var current_page_width = 0;
+var current_page_height = 0;
+
+var width_scale_coeff = 0;
+var height_scale_coeff = 0;
+
+// ******************************* General Material / Big Text / Headers **********************************************
 function DisplayMetrics(props){
   return (
       <h1 className = 'display_metrics'> Metrics {props.m1} {props.m2} {props.m3} {props.m4} </h1>
@@ -26,18 +36,43 @@ function DisplayMetrics(props){
 };
 
 function TextInput(props){
+
+  const text_input_style = {top: (250 * props.current_height / original_page_height) + 'px', 
+                            left:(185 * props.current_width / original_page_width) + 'px',
+                            font: (32 * props.current_width / original_page_width) + 'px Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+
   return (
-      <h1 className = 'text_input'> Input MIDI </h1>
+      <h1 className = 'text_input' style = {text_input_style}> Input MIDI </h1>  
   );
 
 };
 
 function TextOutputVariations(props){
+
+  const text_output_style = {top: (250 * props.current_height / original_page_height) + 'px', 
+                            left:(1425 * props.current_width / original_page_width) + 'px',
+                            font: (32 * props.current_width / original_page_width) + 'px Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+
   return (
-      <h1 className = 'text_output_variations'> Output MIDI (Input Variations) </h1>
+    <h1 className = 'text_output_variations' style = {text_output_style}> Output MIDI (Input Variations) </h1>
   );
 
 };
+
+function ImageComponentArchitecture(props){
+  return (
+    <div className="image_architecture">
+      <img src={props.name} width="943" height="264" />
+    </div>
+  );
+
+};
+
+// ******************************* Pads **********************************************
 
 class Pads extends React.Component{
 
@@ -51,6 +86,8 @@ class Pads extends React.Component{
   }
 
 }
+
+// ******************************* Pad 1**********************************************
 
 class Pad1 extends React.Component {
 
@@ -97,9 +134,10 @@ const PositionLabel1 = (props) => {
         <UpdateGenMedia/>
         <DisplayMetrics m1 = {metric1} m2 = {metric2} m3 = {metric3} m4 = {metric4}/>
         <svg >
-          <circle cx={metric1-10} cy={metric2-10} r="20"/>
+          <circle cx={metric1-10} cy={metric2-10} r="10" fill = 'red'/>
         </svg>
       </ImageBackground>
+      <Pad1ActiveIndicator isActive = {isActive}/>
     </div>
   );
 };
@@ -113,6 +151,25 @@ const ImageComponent = (props) => {
   );
 
 };
+
+function Pad1ActiveIndicator(props){
+
+  if (props.isActive == true){
+    var active_text = 'On';
+    var text_color = 'green';
+  }
+  else{
+    var active_text = 'Off';
+    var text_color = 'red';
+  }
+
+  return(
+  <h1 className = 'pad1_active_indicator' style = {{color: text_color}}> Click on the Pad to Active: {active_text} </h1>
+  );
+}
+
+// ******************************* Pad 2 **********************************************
+
 
 class Pad2 extends React.Component {
   render() {
@@ -156,9 +213,10 @@ const PositionLabel2 = (props) => {
         <UpdateGenMedia/>
         <DisplayMetrics m1 = {metric1} m2 = {metric2} m3 = {metric3} m4 = {metric4}/>
         <svg >
-          <circle cx={metric3-10} cy={metric4-10} r="20" />
+          <circle cx={metric3-10} cy={metric4-10} r="10" fill = 'red'/>
         </svg>
       </ImageBackground>
+      <Pad2ActiveIndicator isActive = {isActive}/>
     </div>
   );
 };
@@ -172,14 +230,25 @@ const ImageComponent2 = (props) => {
 
 };
 
-function ImageComponentArchitecture(props){
-  return (
-    <div className="image_architecture">
-      <img src={props.name} width="943" height="264" />
-    </div>
-  );
+function Pad2ActiveIndicator(props){
 
-};
+  if (props.isActive == true){
+    var active_text = 'On';
+    var text_color = 'green';
+  }
+  else{
+    var active_text = 'Off';
+    var text_color = 'red';
+  }
+
+  return(
+    <h1 className = 'pad2_active_indicator' style = {{color: text_color}}> Click on the Pad to Active: {active_text} </h1>
+  );
+}
+
+
+
+// ********************************* Small Helper Texts ********************************************
 
 class X1_label extends React.Component {
   render() {
@@ -261,34 +330,50 @@ class Pad2_Y1 extends React.Component {
   }
 }
 
-function ImageComponentSourceMusic(props){
-  return (
-    <div className="image_source_music">
-      <img src={`${process.env.PUBLIC_URL}/pianoroll_files/` + props.name} width="550" height="192" />
-      
-    </div>
-  );
-};
+// ************************************** Source Music Pianoroll and Audio Material ****************************************
 
-function ImageComponentGeneratedMusic(props){  
+function ImageComponentSourceMusic(props){
+  const image_source_music_style = {top: (370 * props.current_height / original_page_height) + 'px', 
+                                    left:(-20 * props.current_width / original_page_width) + 'px',
+                                    }
+
   return (
-    <div className="image_generated_music">
-      <img src={`${process.env.PUBLIC_URL}/pianoroll_files/` + props.name} width="550" height="192" />
+    <div className="image_source_music" style = {image_source_music_style}>
+      <img src={`${process.env.PUBLIC_URL}/pianoroll_files/` + props.name} width={550 * props.current_width / original_page_width} height={192 * props.current_height / original_page_height} />
     </div>
   );
 };
 
 function Audio_Player_Source(props){
   var mp3_file_name = props.name;
+  const audio_player_source_style = {top: (600 * props.current_height / original_page_height) + 'px', 
+                                    left:(100 * props.current_width / original_page_width) + 'px',
+                                    }
   
   return (
     <ReactAudioPlayer
         className = "audio_player_source"
         src = {mp3_file_name}
         controls
+        style = {audio_player_source_style}
     />
   );
 }
+
+// ************************************** Generated Music Pianoroll and Audio Material ****************************************
+function ImageComponentGeneratedMusic(props){  
+  const image_generated_music_style = {top: (370 * props.current_height / original_page_height) + 'px', 
+                                      left:(1350 * props.current_width / original_page_width) + 'px',
+                                      }
+
+  return (
+    <div className="image_generated_music" style = {image_generated_music_style}>
+      <img src={`${process.env.PUBLIC_URL}/pianoroll_files/` + props.name} width={550 * props.current_width / original_page_width} height={192 * props.current_height / original_page_height} />
+    </div>
+  );
+};
+
+
 
 function Audio_Player_Generated(props){
   return (
@@ -320,15 +405,59 @@ function UpdateGenMedia(){
 }
 
 
+// ************************************** App ****************************************
+
+class CreateContact extends React.Component {
+  state = {
+    windowHeight: undefined,
+    windowWidth: undefined
+  }
+
+  handleResize = () => this.setState({
+    windowHeight: window.innerHeight,
+    windowWidth: window.innerWidth
+  });
+
+  componentDidMount() {
+    this.handleResize();
+    
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize)
+  }
+  
+  current_page_width = this.state.windowWidth;
+  current_page_height = this.state.windowHeight;
+
+  render() {
+    return (
+      <div>
+
+        <Audio_Player_Source name = {"./mp3_files/" + source_mp3_file_name} current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+
+
+        <ImageComponentSourceMusic name = {source_pianoroll_file_name} current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+
+        <TextInput current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+        <TextOutputVariations current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+
+
+      </div>
+    );
+  }
+}
+
+
 function App() {
   
   return (
     <div className="App">
-      <Audio_Player_Source name = {"./mp3_files/" + source_mp3_file_name} />
+      
       <Pads/>
       {/* <Pad1 />
       <Pad2 /> */}
-      
       <X1_label />
       <Y1_label />
       <X2_label />
@@ -340,10 +469,12 @@ function App() {
       <Pad2_X1/>
       <Pad2_Y1/>
       <ImageComponentArchitecture name = {image_architecture}/>
-      <ImageComponentSourceMusic name = {source_pianoroll_file_name}/>
       
-      <TextInput/>
-      <TextOutputVariations/>
+      
+      
+
+      <CreateContact />
+
 
       
     </div>
