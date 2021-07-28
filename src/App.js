fd8d0bcfb8c0f5f60880b2e6087e027ab023a3c8
bbanar3 <88082353+bbanar3@users.noreply.github.com>
2021-07-28 17:45:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import pad_image_1 from "./color_2d_plot1.png";
 import pad_image_2 from "./color_2d_plot2.png";
 import image_architecture from "./figure_with_LSR.png";
 import ReactAudioPlayer from 'react-audio-player';
-import { ImageBackground} from "react-native";
+import { ImageBackground, TextPropTypes} from "react-native";
 import { useState, useEffect } from 'react';
 
 
@@ -64,9 +64,12 @@ function TextOutputVariations(props){
 };
 
 function ImageComponentArchitecture(props){
+  const image_architecture_style = {top: (0 * props.current_height / original_page_height) + 'px', 
+                                      left:(500 * props.current_width / original_page_width) + 'px',
+                                      }
   return (
-    <div className="image_architecture">
-      <img src={props.name} width="943" height="264" />
+    <div className="image_architecture" style = {image_architecture_style}>
+      <img src={props.name} width= {943 * props.current_width / original_page_width} height={264 * props.current_height / original_page_height} />
     </div>
   );
 
@@ -171,17 +174,23 @@ function Pad1ActiveIndicator(props){
 // ******************************* Pad 2 **********************************************
 
 
-class Pad2 extends React.Component {
-  render() {
-    return (
-      <ReactCursorPosition className="pad2"
-       activationInteractionMouse={INTERACTIONS.CLICK}
-      >
-        <PositionLabel2 />
-        {/* <ImageComponent2 /> */}
-      </ReactCursorPosition>
-    );
-  }
+function Pad2(props) {
+
+  const pad2_style = {top: (350 * props.current_height / original_page_height) + 'px', 
+                      left:(1000 * props.current_width / original_page_width) + 'px',
+                      width: (300 * props.current_width / original_page_width) + 'px',
+                      height: (300 * props.current_height / original_page_height) + 'px',
+                      }
+
+  return (
+    <ReactCursorPosition className="pad2"
+      activationInteractionMouse={INTERACTIONS.CLICK}
+      style = {pad2_style}
+    >
+      <PositionLabel2 current_height = {props.current_height} current_width = {props.current_width}/>
+      {/* <ImageComponent2 current_height = {props.current_height} current_width = {props.current_width}/> */}
+    </ReactCursorPosition>
+  );
 }
 
 const PositionLabel2 = (props) => {
@@ -207,16 +216,20 @@ const PositionLabel2 = (props) => {
     metric4 = y;
   };
 
+  const pad2_background_image_style = {height: (290 * props.current_height / original_page_height) + 'px', 
+                                      width:(290 * props.current_width / original_page_width) + 'px',
+                                      }
+
   return (
     <div className="pad2__label">
-      <ImageBackground source = {pad_image_2} style={{width: '100%', height: '194%'}}>
-        <UpdateGenMedia/>
+      <ImageBackground source = {pad_image_2} style={pad2_background_image_style}>
+        <UpdateGenMedia current_height = {props.current_height} current_width = {props.current_width}/>
         <DisplayMetrics m1 = {metric1} m2 = {metric2} m3 = {metric3} m4 = {metric4}/>
         <svg >
-          <circle cx={metric3-10} cy={metric4-10} r="10" fill = 'red'/>
+          <circle cx={metric3 - 10 * props.current_width / original_page_width} cy={metric4 - 10 * props.current_height / original_page_height} r={10 * props.current_width / original_page_width} fill = 'red'/>
         </svg>
       </ImageBackground>
-      <Pad2ActiveIndicator isActive = {isActive}/>
+      <Pad2ActiveIndicator isActive = {isActive} current_height = {props.current_height} current_width = {props.current_width}/>
     </div>
   );
 };
@@ -231,7 +244,6 @@ const ImageComponent2 = (props) => {
 };
 
 function Pad2ActiveIndicator(props){
-
   if (props.isActive == true){
     var active_text = 'On';
     var text_color = 'green';
@@ -241,8 +253,15 @@ function Pad2ActiveIndicator(props){
     var text_color = 'red';
   }
 
+  const pad2_active_indicator_style = {top: (300 * props.current_height / original_page_height) + 'px', 
+                                      left:(1015 * props.current_width / original_page_width) + 'px',
+                                      font: (18 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                                      fontWeight: 'bold',
+                                      color: text_color,
+                                      }
+
   return(
-    <h1 className = 'pad2_active_indicator' style = {{color: text_color}}> Click on the Pad to Active: {active_text} </h1>
+    <h1 className = 'pad2_active_indicator' style = {pad2_active_indicator_style}> Click on the Pad to Active: {active_text} </h1>
   );
 }
 
@@ -250,84 +269,120 @@ function Pad2ActiveIndicator(props){
 
 // ********************************* Small Helper Texts ********************************************
 
-class X1_label extends React.Component {
-  render() {
-    return (
-      <h1 className = 'x1_label' > Rhythmic Complexity Level <br/> Latent Dimension = 0</h1>
-    );
-  }
+function X1_label(props) {
+  const x1_label_style = {top: (650 * props.current_height / original_page_height) + 'px', 
+                            left:(640 * props.current_width / original_page_width) + 'px',
+                            font: (16 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+
+
+  return (
+    <h1 className = 'x1_label' style = {x1_label_style}> Rhythmic Complexity Level <br/> Latent Dimension = 0</h1>
+  );
 }
 
-class Y1_label extends React.Component {
-  render() {
-    return (
-      <h1 className = 'y1_label' >Note Range Level <br/> Latent Dimension = 1</h1>
-    );
-  }
+function Y1_label(props) {
+  const y1_label_style = {top: (475 * props.current_height / original_page_height) + 'px', 
+                            left:(480 * props.current_width / original_page_width) + 'px',
+                            font: (16 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+
+  return (
+    <h1 className = 'y1_label' style = {y1_label_style}> Note Range Level <br/> Latent Dimension = 1</h1>
+  );
+
 }
 
-class X2_label extends React.Component {
-  render() {
-    return (
-      <h1 className = 'x2_label'>Note Density Level <br/> Latent Dimension = 2</h1>
-    );
-  }
+function X2_label(props) {
+  const x2_label_style = {top: (650 * props.current_height / original_page_height) + 'px', 
+                            left:(1060 * props.current_width / original_page_width) + 'px',
+                            font: (16 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+  
+  return (
+    <h1 className = 'x2_label' style = {x2_label_style}>Note Density Level <br/> Latent Dimension = 2</h1>
+  );
 }
 
-class Y2_label extends React.Component {
-  render() {
-    return (
-      <h1 className = 'y2_label'>Average Pitch Interval Level <br/> Latent Dimension = 3</h1>
-    );
-  }
+function Y2_label(props){
+  const y2_label_style = {top: (475 * props.current_height / original_page_height) + 'px', 
+                            left:(860 * props.current_width / original_page_width) + 'px',
+                            font: (16 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+  
+  return (
+    <h1 className = 'y2_label' style = {y2_label_style}>Average Pitch Interval Level <br/> Latent Dimension = 3</h1>
+  );
 }
 
-class Pad1_X0 extends React.Component {
-  render() {
-    return (
-      <h1 className = 'pad1_x0'>0</h1>
-    );
-  }
+function Pad1_X0(props) {
+  const pad1_x0_style = {top: (640 * props.current_height / original_page_height) + 'px', 
+                            left:(590 * props.current_width / original_page_width) + 'px',
+                            font: (16 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+  return (
+    <h1 className = 'pad1_x0' style = {pad1_x0_style} >0</h1>
+  );
 }
 
-class Pad1_X1 extends React.Component {
-  render() {
-    return (
-      <h1 className = 'pad1_x1'>10</h1>
-    );
-  }
+function Pad1_X1(props) {
+  const pad1_x1_style = {top: (640 * props.current_height / original_page_height) + 'px', 
+                            left:(900 * props.current_width / original_page_width) + 'px',
+                            font: (16 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+  return (
+    <h1 className = 'pad1_x1' style = {pad1_x1_style}>10</h1>
+  );
 }
 
-class Pad1_Y1 extends React.Component {
-  render() {
-    return (
-      <h1 className = 'pad1_y1'>10</h1>
-    );
-  }
+function Pad1_Y1(props) {
+  const pad1_y1_style = {top: (320 * props.current_height / original_page_height) + 'px', 
+                            left:(580 * props.current_width / original_page_width) + 'px',
+                            font: (16 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+  return (
+    <h1 className = 'pad1_y1' style = {pad1_y1_style}>10</h1>
+  );
 }
 
-class Pad2_X0 extends React.Component {
-  render() {
-    return (
-      <h1 className = 'pad2_x0'>0</h1>
-    );
-  }
+function Pad2_X0(props) {
+  const pad2_x0_style = {top: (640 * props.current_height / original_page_height) + 'px', 
+                            left:(990 * props.current_width / original_page_width) + 'px',
+                            font: (16 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+  return (
+    <h1 className = 'pad2_x0' style = {pad2_x0_style}>0</h1>
+  );
 }
 
-class Pad2_X1 extends React.Component {
-  render() {
-    return (
-      <h1 className = 'pad2_x1'>10</h1>
-    );
-  }
+function Pad2_X1(props) {
+  const pad2_x1_style = {top: (640 * props.current_height / original_page_height) + 'px', 
+                            left:(1300 * props.current_width / original_page_width) + 'px',
+                            font: (16 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+  return (
+    <h1 className = 'pad2_x1' style = {pad2_x1_style}>10</h1>
+  );
 }
 
-class Pad2_Y1 extends React.Component {
-  render() {
-    return (
-      <h1 className = 'pad2_y1'>10</h1>
-    );
-  }
+function Pad2_Y1(props) {
+  const pad2_y1_style = {top: (320 * props.current_height / original_page_height) + 'px', 
+                            left:(980 * props.current_width / original_page_width) + 'px',
+                            font: (16 * props.current_width / original_page_width) + 'px/1.5 Helvetica, Arial',
+                            fontWeight: 'bold',
+                            }
+  return (
+    <h1 className = 'pad2_y1' style = {pad2_y1_style}>10</h1>
+  );
 }
 
 // ************************************** Source Music Pianoroll and Audio Material ****************************************
@@ -347,8 +402,9 @@ function ImageComponentSourceMusic(props){
 function Audio_Player_Source(props){
   var mp3_file_name = props.name;
   const audio_player_source_style = {top: (600 * props.current_height / original_page_height) + 'px', 
-                                    left:(100 * props.current_width / original_page_width) + 'px',
-                                    }
+                                    left:(55 * props.current_width / original_page_width) + 'px',
+                                    width: (400 * props.current_width / original_page_width) + 'px',
+                                    height: (50 * props.current_width / original_page_width) + 'px'}
   
   return (
     <ReactAudioPlayer
@@ -373,19 +429,22 @@ function ImageComponentGeneratedMusic(props){
   );
 };
 
-
-
 function Audio_Player_Generated(props){
+  const audio_player_generated_style = {top: (600 * props.current_height / original_page_height) + 'px', 
+                                    left:(1500 * props.current_width / original_page_width) + 'px',
+                                    width: (400 * props.current_width / original_page_width) + 'px',
+                                    height: (50 * props.current_width / original_page_width) + 'px'}
   return (
     <ReactAudioPlayer
         className = "audio_player_generated"
         src = {`${process.env.PUBLIC_URL}/mp3_files/` + props.name}
         controls
+        style = {audio_player_generated_style}
     />
   );
 }
 
-function UpdateGenMedia(){
+function UpdateGenMedia(props){
 
   var metric1_quantized = Math.floor(metric1 / 30); 
   var metric2_quantized = Math.floor(metric2 / 30); 
@@ -398,12 +457,11 @@ function UpdateGenMedia(){
 
   return(
     <div className = "updateGenMedia">
-      <ImageComponentGeneratedMusic name = {gen_pianoroll_file_name}/>
-      <Audio_Player_Generated name = {gen_mp3_file_name} />
+      <ImageComponentGeneratedMusic name = {gen_pianoroll_file_name} current_height = {props.current_height} current_width = {props.current_width}/>
+      <Audio_Player_Generated name = {gen_mp3_file_name} current_height = {props.current_height} current_width = {props.current_width}/>
     </div>
   );
 }
-
 
 // ************************************** App ****************************************
 
@@ -434,15 +492,28 @@ class CreateContact extends React.Component {
   render() {
     return (
       <div>
+        {/* <Pad1 /> */}
+        <Pad2 current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+
+
+        <X1_label current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+        <Y1_label current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+        <X2_label current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+        <Y2_label current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+        <Pad1_X0 current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+        <Pad1_X1 current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+        <Pad1_Y1 current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+        <Pad2_X0 current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+        <Pad2_X1 current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
+        <Pad2_Y1 current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
 
         <Audio_Player_Source name = {"./mp3_files/" + source_mp3_file_name} current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
-
-
         <ImageComponentSourceMusic name = {source_pianoroll_file_name} current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
 
         <TextInput current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
         <TextOutputVariations current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
 
+        <ImageComponentArchitecture name = {image_architecture} current_height = {this.state.windowHeight} current_width = {this.state.windowWidth}/>
 
       </div>
     );
@@ -454,31 +525,11 @@ function App() {
   
   return (
     <div className="App">
-      
-      <Pads/>
-      {/* <Pad1 />
-      <Pad2 /> */}
-      <X1_label />
-      <Y1_label />
-      <X2_label />
-      <Y2_label />
-      <Pad1_X0/>
-      <Pad1_X1/>
-      <Pad1_Y1/>
-      <Pad2_X0/>
-      <Pad2_X1/>
-      <Pad2_Y1/>
-      <ImageComponentArchitecture name = {image_architecture}/>
-      
-      
-      
-
+      {/* <Pads/> */}
       <CreateContact />
-
-
-      
     </div>
   );
 }
 
 export default App;
+
